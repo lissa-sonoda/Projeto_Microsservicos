0676318d77d2ab62f3a3db39235a054375a15e62
Helen Lissa Sonoda <helen.lissa@gmail.com>
2021-10-02 10:39:28 -0300

    Adicionando os microsservicos

---
+++ lembrete/src/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const axios = require("axios");
+//const bodyParser = require("body-parser");
+
+const app = express();
+//app.use(bodyParser.json());
+app.use(express.json()); //pode ser assim
+
+const lembretes = {};
+contador = 0;
+
+//método para obter os lembretes
+app.get("/lembrete", (req, res) => {
+  res.send(lembretes);
+});
+
+//método para inserir/criar um novo lembrete
+app.put("/lembrete", async (req, res) => {
+  contador++;
+  const { texto } = req.body;
+  lembretes[contador] = {
+    contador,
+    texto,
+  };
+  await axios.post("http://localhost:10000/eventos", {
+    tipo: "LembreteCriado",
+    dados: {
+      contador,
+      texto,
+    },
+  });
+  res.status(201).send(lembretes[contador]);
+});
+
+app.post("/eventos", (req, res) => {
+  console.log(req.body);
+  res.status(200).send({ msg: "ok" });
+});
+
+//porta
+app.listen(4000, () => {
+  console.log("Lembretes. Porta 4000");
+});
---
+++ observacao/src/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
+//const bodyParser = require("body-parser");
+
+const app = express();
+//app.use(bodyParser.json());
+app.use(express.json());
+
+const observacoesPorLembreteId = {};
+
+const funcoes = {
+  ObservacaoClassificada: (observacao) => {
+    const observacoes = observacoesPorLembreteId[observacao.lembreteId];
+    const obsParaAtualizar = observacoes.find((o) => o.id === observacao.id);
+    obsParaAtualizar.status = observacao.status;
+    axios.post("http://localhost:10000/eventos", {
+      tipo: "ObservacaoAtualizada",
+      dados: {
+        id: observacao.id,
+        texto: observacao.texto,
+        lembreteId: observacao.lembreteId,
+        status: observacao.status,
+      },
+    });
+  },
+};
+
+app.get("/lembrete/:id/observacao", (req, res) => {
+  res.send(observacoesPorLembreteId[req.params.id] || []);
+});
+
+//:id é um placeholder
+app.put("/lembrete/:id/observacao", async (req, res) => {
+  const idObs = uuidv4();
+  const { texto } = req.body;
+  //req.params dá acesso à lista de parâmetros da URL
+  const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
+  //observacoesDoLembrete.push({ id: idObs, texto }); - antes
+  observacoesDoLembrete.push({ id: idObs, texto, status: "aguardando" }); //atualização referente ao microsserviço de classificação
+  observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+  await axios.post("http://localhost:10000/eventos", {
+    tipo: "ObservacaoCriada",
+    dados: {
+      id: idObs,
+      texto,
+      lembreteId: req.params.id,
+      status: "aguardando",
+    },
+  });
+  res.status(201).send(observacoesDoLembrete);
+});
+
+app.post("/eventos", (req, res) => {
+  //console.log(req.body);
+  //funcoes[req.body.tipo](req.body.dados);
+  //res.status(200).send({ msg: "ok" });
+
+  try {
+    funcoes[req.body.tipo](req.body.dados);
+  } catch (err) {}
+  res.status(200).send({ msg: "ok" });
+});
+
+app.listen(5000, () => {
+  console.log("Lembretes. Porta 5000");
+});
